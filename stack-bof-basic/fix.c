3734daf7a60ae336d5868ea04cf59098ecd181c3
sage <jjhcat0402@gmail.com>
2026-01-04 15:06:45 +0900

    Fix buffer overflow vulnerability in vuln function
    Replaced unsafe 'gets' function with 'fgets' to prevent buffer overflow.

---
+++ stack-bof-basic/fix.c
@@ -0,0 +1,22 @@
+//filename: fix.c
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+void win(){
+    setregid(getegid(), getegid());
+    system("/bin/sh");
+}
+
+void vuln(int value){
+    char buf[16];
+    printf("input: ");
+    fgets(buf, sizeof(buf), stdin);
+    printf("value: %d\n", value);
+    printf("buf: %s\n", buf);
+}
+
+int main(){
+    int num = 5;
+    vuln(num);
+}
