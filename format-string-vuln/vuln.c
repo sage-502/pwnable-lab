1bcb4da314afee4a1147b8de64563eb2bcbb9fdb
sage <jjhcat0402@gmail.com>
2026-01-08 18:20:39 +0900

    Update target variable and printf format
    Change target variable initialization and format output.

--- format-string-vuln/vuln.c
+++ format-string-vuln/vuln.c
@@ -1,15 +1,20 @@
 //filename: vuln.c
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int target = 0;
+    int target = 0xcafebabe;
     char buf[100];
 
     printf("target addr = %p\n", &target);
     fgets(buf, sizeof(buf), stdin);
     printf(buf);
-    printf("\ntarget = %d\n", target);
+    printf("\ntarget = 0x%x\n", target);
+
+    if(target == 0xdeadbeef){
+	printf("good!\n");
+        system("/bin/bash");
+    }	    
 
     return 0;
 }
