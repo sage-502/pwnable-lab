c5768b59edd7acf6f0b036eb3ca55c8bc0a67624
sage <jjhcat0402@gmail.com>
2026-01-26 20:01:05 +0900

    Add vulnerability demonstration in fix.c

---
+++ bof-ret2libc-basic/fix.c
@@ -0,0 +1,18 @@
+// filename: vuln.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void vuln() {
+    char buf[24];
+    puts("input:");
+    fgets(buf, sizeof(buf), stdin);
+}
+
+int main() {
+    setregid(getegid(), getegid());
+    setvbuf(stdout, NULL, _IONBF, 0);
+    vuln();
+    puts("done");
+    return 0;
+}
