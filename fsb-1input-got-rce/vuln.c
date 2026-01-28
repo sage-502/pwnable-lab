13a536ad97f71554d4c67c646b834dd82968e870
sage <jjhcat0402@gmail.com>
2026-01-28 18:13:41 +0900

    Create vuln.c

---
+++ fsb-1input-got-rce/vuln.c
@@ -0,0 +1,19 @@
+// filename: vuln.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    char buf[200];
+
+    setregid(getegid(), getegid());
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    puts("input:");
+    fgets(buf, sizeof(buf), stdin);
+
+    printf(buf);
+    puts(buf);
+
+    return 0;
+}
